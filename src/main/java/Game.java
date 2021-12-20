5820dc493d7fba69bdad0d33d704b9f396dc829b
Francisco Pimentel Serra <FranciscoSerra@MBP-de-Carolina.Home>
2021-12-20 12:09:14 +0000

    Small changes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,5 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
