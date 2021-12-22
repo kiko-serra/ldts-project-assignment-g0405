dccaf1b705a92b1a6a435664c61a7c08bf3c0646
up201706546 <up201706546@fe.up.pt>
2021-12-22 16:18:43 +0000

    First Tests2

---
+++ src/test/java/test_Components.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test_Components {
+
+    @Test
+    public void test_borders(){
+        Borders borders = new Borders(10,20);
+
+        assertEquals(10,borders.getPosition().getX());
+        assertEquals(20,borders.getPosition().getY());
+
+
+        Borders borders1 = new Borders(20,10);
+
+        borders.setPosition(borders1.getPosition());
+
+        assertEquals(20,borders.getPosition().getX());
+        assertEquals(10,borders.getPosition().getY());
+
+    }
+
+    @Test
+    public void test_jack_the_sparrow(){
+        Jack_The_Sparrow jack = new Jack_The_Sparrow(10,20);
+
+        assertEquals(10,jack.getPosition().getX());
+        assertEquals(20,jack.getPosition().getY());
+    }
+}
