762c0e74f8583860b0eaf269c7543492cc691853
up201706546 <up201706546@fe.up.pt>
2022-01-08 19:08:16 +0000

    Changes

--- src/test/java/testMovements.java
+++ src/test/java/testMovements.java
@@ -39,22 +39,22 @@ public class testMovements {
 
         Pirates pirates1 = new Pirates(1, 20);
         pirates1.setState(1);
-        pirates1.canPirateMove(pirates1, 30);
+        pirates1.canPirateMove(30);
         assertEquals(0, pirates1.getState());
 
         Pirates pirates4 = new Pirates(18, 4);
         pirates4.setState(1);
-        pirates4.canPirateMove(pirates4, 30);
+        pirates4.canPirateMove(30);
         assertEquals(0, pirates4.getState());
 
         Pirates pirates2 = new Pirates(28, 4);
         pirates2.setState(0);
-        pirates2.canPirateMove(pirates2, 30);
+        pirates2.canPirateMove(30);
         assertEquals(1, pirates2.getState());
 
         Pirates pirates3 = new Pirates(12, 4);
         pirates3.setState(0);
-        pirates3.canPirateMove(pirates3, 30);
+        pirates3.canPirateMove(30);
         assertEquals(1, pirates3.getState());
     }
 }
